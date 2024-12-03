c540305052fc0bdca045f6c00db29b090ba209f1
Gaëlle Dumont Le Brazidec <gaelledumontlb@gmail.com>
2024-12-03 19:42:48 +0100

    update

--- model/Model.py
+++ model/Model.py
@@ -1,7 +1,7 @@
 import torch
 import torch as nn
 from Transformer import Transformer
-from embedding import CamemBERTEmbedding
+from preprocessing.embedding import CamemBERTEmbedding
 
 class Model(nn.Module):
     def __init__(self, device, vocab_size, max_len, input_dim, embed_dim=512, hidden_dim=768, num_heads=12, batch_size=32, dropout=0.1, layers=12):
