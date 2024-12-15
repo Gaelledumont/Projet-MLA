924a3e4bf58c2b439a3bfffa1273fce4980a79b0
Tony Cao <tcao@protonmail.com>
2024-12-15 23:43:11 +0100

    Loss

---
+++ model/CamemBERTForMaskedLM.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+from CamemBERTModel import CamemBERTModel
+
+class CamemBERTForMaskedKM(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.camembert = CamemBERTModel(config)
+        self.lm_head = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
+        self.lm_bias = nn.Parameter(torch.zeros(config.vocab_size))
+        self.lm_head.bias = self.lm_bias
+
+    def forward(self, input_ids, labels=None):
+        hidden_states = self.camembert(input_ids)
+        logits = self.lm_head(hidden_states)
+
+        loss = None
+        if labels is not None:
+            loss_fct = nn.CrossEntropyLoss(ignore_index=-100)
+            loss = loss_fct(logits.view(-1, logits.size(-1)), labels.view(-1))
+        return logits, loss
