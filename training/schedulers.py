ee06706aa7dfafec1653ed862ea324a9a0ab1682
Tony Cao <tcao@protonmail.com>
2024-12-22 11:52:34 +0100

    debuggage

--- training/schedulers.py
+++ training/schedulers.py
@@ -1,10 +1,11 @@
 class PolynomialDecayLR:
-    def __init__(self, optimizer, warmup_steps, total_steps, end_learning_rate=0.0, power=1.0):
+    def __init__(self, optimizer, warmup_steps, total_steps, end_learning_rate=0.0, power=1.0, last_epoch=-1):
         self.optimizer = optimizer
         self.warmup_steps = warmup_steps
         self.total_steps = total_steps
         self.end_learning_rate = end_learning_rate
         self.power = power
+        self.last_epoch = last_epoch
         self.current_step = 0
         self.base_lrs = [group['lr'] for group in optimizer.param_groups]
 
@@ -15,8 +16,8 @@ class PolynomialDecayLR:
 
     def get_lr(self, param_group_idx):
         if self.current_step < self.warmup_steps:
-            return self.base_lrs[param_group_idx]*(self.current_step/self.warmup_steps)
+            return self.base_lrs[param_group_idx]*self.current_step/self.warmup_steps
         else:
-            progress = (self.current_step - self.warmup_steps)/(self.total_steps - self.warmup_steps)
+            progress = (self.current_step - self.warmup_steps) / (self.total_steps - self.warmup_steps)
             decay = (1 - progress)**self.power
             return (self.base_lrs[param_group_idx] - self.end_learning_rate)*decay + self.end_learning_rate
