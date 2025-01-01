6bb432efc24f83e788a839c267573645dae2e211
Tony Cao <tcao@protonmail.com>
2025-01-01 01:20:36 +0100

    Typos

--- tokenization/train_sentencepiece.py
+++ tokenization/train_sentencepiece.py
@@ -12,7 +12,7 @@ def train_sentencepiece(corpus_path, model_prefix="spm", vocab_size=32000):
         f"--vocab_size={vocab_size} "
         f"--character_coverage=0.9995 "
         f"--model_type=bpe"
-        f"--unk-id=0 --pad_id=1 --bos_id=2 --eos_id=3 "
+        f"--unk_id=0 --pad_id=1 --bos_id=2 --eos_id=3 "
         f"--user_defined_symbols=<mask> "
         f"--hard_vocab_limit=false"
         f"--num_threads=8 "
